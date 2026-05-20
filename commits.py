ffc91d42b294f2071dd40bfdd2dd2c600232a727
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-20 22:24:18 +0530

    Update commits.py

--- commits.py
+++ commits.py
@@ -1,7 +1,6 @@
 ::view-transition-old(*)
 ::view-transition-new(*) 
-                        
-    animation-duration: 0.25s;
+animation-duration: 0.25s;
 
     animation-timing-function: cubic-bezier(0.19, 1, 0.22, 1);
   }
