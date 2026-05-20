b70d595a391a4ef1517cd3ef384ac8ddfbd372d7
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-20 22:24:57 +0530

    Update commits.py

--- commits.py
+++ commits.py
@@ -6,7 +6,6 @@ animation-timing-function: cubic-bezier(0.19, 1, 0.22, 1);
 VvisualizeVvisualize show_widgetModule 02
 — How it works. Every thought, movement, and
 feeling is an electrical signal travelling down
-
 an axon and jumping across a synapse via chemical
 messengers called neurotransmitters. Myelin — the
 fatty white sheath — is the brain's insulation,
