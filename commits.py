22f547f1adb6c15a2ccb81c93634ba0bf106b223
Ayush Burman <72551504+Ayushburman@users.noreply.github.com>
2026-05-20 22:25:59 +0530

    Update commits.py

--- commits.py
+++ commits.py
@@ -3,6 +3,7 @@
 ::view-transition-new(*) 
 
 animation-duration: 0.25s;
+
 animation-timing-function: cubic-bezier(0.19, 1, 0.22, 1);
 VvisualizeVvisualize show_widgetModule 02
 — How it works. Every thought, movement, and
